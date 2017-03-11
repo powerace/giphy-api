f271ff8b48a99cc033cf36319bdd3b8b798fce23
Taniesha Robinson <taniesharobinson@mcs-macbook06.att.net>
2017-03-11 09:04:35 -0600

    buttons and search form working

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,62 @@
+var searchButtons = ["aardvark", "beagle", "cobra"];
 
-var searchTerm = $('#search').val();
+//build and append all buttons
+for (var i = 0; i < searchButtons.length; i++){
+	buildButton(searchButtons[i]);
+}
 
-var url = "http://api.giphy.com/v1/gifs/search";
-url += '?' + $.param({
-  'api_key': "dc6zaTOxFJmzC",
-   'q': searchTerm,
-   'limit': 10,
-   'rating': "pg-13"
+//add button
+$("button[type='submit']").on("click", function() {
+	event.preventDefault();
+	if ($('#name').val() != ''){
+		buildButton($('#name').val());
+	} 
+	
 });
 
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
+//search with buttons
+$(".searchButtons").on("click", "button.animal", function() {
+	$(".searchResults").empty();
+    var searchTerm = $(this).text().trim();
+	var url = "http://api.giphy.com/v1/gifs/search";
+	url += '?' + $.param({
+	  'api_key': "dc6zaTOxFJmzC",
+	   'q': searchTerm,
+	   'limit': 10,
+	   'rating': "g"
+	});
+
+	$.ajax({
+	  url: url,
+	  method: 'GET',
+	}).done(function(response) {
+	  console.log(response);
+	  var results = response.data;
+	  displayResults(results);
+	}).fail(function(err) {
+	  throw err;
+	});
 });
+
+function displayResults(results){
+	for (var i = 0; i < results.length; i++) {
+	    var gifDiv = $("<div class='animalItem'>");
+
+	    var rating = results[i].rating;
+
+	    var p = $("<p>").text("Rating: " + rating);
+
+	    var animalImage = $("<img>");
+	    animalImage.attr("src", results[i].images.fixed_height.url);
+
+	    gifDiv.append(p).append(animalImage);
+
+	    $(".searchResults").append(gifDiv);
+	  }
+}
+
+function buildButton(name){
+	
+	$('.searchButtons').append('<button class="animal btn btn-primary">' + name + '</button>');
+	
+}
